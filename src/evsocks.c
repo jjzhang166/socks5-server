3617230a581b30ee3f21c726040ff85f48c524fb
xun <lu.xun@gmx.com>
2017-11-22 01:44:23 +0000

    Almost back to an initial commit
    Changed most of functions. It is important
    to handle unused buffers in libeven and drain them
    to prevent memory overflow or something that will cuase memory issues.
    If wrong evbuffer_free usages detected, program will be terminated. I learnt
    it previous hack.
    
    TODO:
      handle new buffer after the initial socks requests
      from clients
    
      learn proper usages of evbuffer_free, when, how, why...
      etc... otherwise program will crash!

--- src/evsocks.c
+++ src/evsocks.c
@@ -18,6 +18,9 @@ server replies:
 #program clang diagnostic ignored "-Wdeprecated-declarations"
 #endif
 
+#include <assert.h>
+#include "internal.h"
+
 #ifdef _WIN32
 #include <winsock2.h>
 #include <ws2tcip.h>
@@ -27,6 +30,7 @@ server replies:
 #include <arpa/inet.h>
 #endif
 
+#include <signal.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
@@ -36,298 +40,295 @@ server replies:
 #include <event2/util.h>
 #include <event2/event.h>
 
-#include "internal.h"
-#include "evfunc.h"
+#define SOCKS_VERSION 5
+ /* address type */
+#define IPV4 1
+#define IPV6 4
+#define _DOMAINNAME 3
 
+ 
+static struct event_base *base;
+static void syntax(void);
+static void accept_func(struct evconnlistener *listener, evutil_socket_t fd, struct sockaddr *a, int slen, void *p);
+static void read_func(struct bufferevent *bev, void *ctx);
+static void event_func(struct bufferevent *bev, short what, void *ctx);
+static void handle_connect(struct bufferevent *bev, unsigned char *buffer, ev_ssize_t esize);
+
+struct addrspec *
+handle_addrspec(unsigned char * buffer);
+char * get_socks_header(char cmd);
+
+struct addrspec {
+  short sin_family;
+  unsigned char *domain;
+  unsigned long s_addr;
+  unsigned char *_s6_addr;
+  unsigned short port; /* load with shift operator. otherwise endianness will be messed.. */
+};
+
+char *
+get_socks_header(char cmd)
+{
+  static char buffer[9];
+  buffer[0] = 5;
+  buffer[0] = cmd;   
+  buffer[0] = 0;
+  buffer[0] = 1;
+  
+  buffer[0] = 0;
+  buffer[0] = 0;
+  buffer[0] = 0;
+  buffer[0] = 0;
+  
+  buffer[0] = 0;
+  buffer[0] = 0;
+  
+  return buffer;
+}
+
+/* event status */  
+/* handling data = 1 */
+/* writing data to dst = 2*/
+static int status = 0;
 
 static void
-reader_func(struct bufferevent *bev, void *ctx)
+read_func(struct bufferevent *bev, void *ctx)
 {
-  struct bufferevent *partner;
-  struct evbuffer *src;  
+  struct evbuffer *src;
+  ev_ssize_t esize;
   size_t len;
-  static int times = 0;
-  ev_ssize_t evsize;
   
-  partner = ctx;
+  /* this is definitely temporary */
+  unsigned char payload[12] = {5, 0, 5, 0, 0, 1, 0, 0, 0, 0, 0, 0};
+  
   src = bufferevent_get_input(bev);
-  len = evbuffer_get_length(src); 
+  len = evbuffer_get_length(src);
   
-  if (len != 0) { /* got some kind of request */
-      times++;
-      printf("length: %ld, called %d time(s)\n", len, times);
-      /* send data depending on the reply */
-      unsigned char buffer[len]; // got output from partner
-      evsize = evbuffer_copyout(src, buffer, len);
+  unsigned char buffer[len];
+  esize = evbuffer_copyout(src, buffer, len);
+
+  if (buffer[0] == SOCKS_VERSION) {
+    printf("  [INFO: get request]\n");
+    printf("  [INFO: buf size=%ld]\n", esize);
+    
+    /* start to parse data */
+    switch (buffer[1]) {
+    case CONNECT:
+      puts("connect");
+      evbuffer_drain(src, 3); /* cut 3 bytes */
+      handle_connect(bev, buffer, esize);
+      break;
+    case BIND:
+      puts("bind");
+      evbuffer_drain(src, 4); /* cut 4 bytes */
+      handle_connect(bev, buffer, esize);      
+      break;
+    case UDPASSOC:
+      puts("udp associate");
+      // handle_udpassoc();
+      break;
+    default:
+      fprintf(stderr, "[ERROR: read_func.switch cmd not supported]\n");
+    }
+    /* send an initial reply */
+    if (bufferevent_write(bev, payload, 12)<0) /* send out ack reply */
+      fprintf(stderr, "read_func.bufferevent_write\n");
+    
+    /* set statis to reading */
+    status = 1;
+
+  } else if (status == 1) {
+    printf("esize=%ld; len=%ld\n\n", esize, len);
+    for (int i = 0; i < len; i++)
+      printf("%c", buffer[i]);
+    puts(" ");
+    status = 0;
+  } else {
+    bufferevent_free(bev);
+    bufferevent_disable(bev, EV_READ);
+  }
+  
+}
+
+static void
+close_on_flush(struct bufferevent *bev, void *ctx)
+{
+  struct evbuffer *b = bufferevent_get_output(bev);
+
+  if (evbuffer_get_length(b) == 0) {
+    puts("close_on_flush.flush");
+    bufferevent_free(bev);
+  } else {
+    puts("data to read close_on_flush");
+  }
+}
+
+static void
+event_func(struct bufferevent *bev, short what, void *ctx)
+{
+  struct bufferevent *evbuf = ctx;
       
-      if (buffer[0] != (uint8_t) SOCKS_VERSION) {
-	printf("version is so wrong: %d\n", buffer[0]);
-	bufferevent_free(bev);
+  if (what & (BEV_EVENT_READING|BEV_EVENT_WRITING|BEV_EVENT_ERROR)) {
+    if (what & BEV_EVENT_ERROR) {
+      if (errno)
+	perror("connection error");
+      bufferevent_free(evbuf);
+      bufferevent_disable(evbuf, EV_READ|EV_WRITE);
+    }
+    
+    if (what & BEV_EVENT_READING) {
+      puts("[INFO event_func.reading]");
+    }
+    
+    if (what & BEV_EVENT_WRITING) {
+      puts("[INFO event_func.WRITING]");
+    }
+    
+    if (evbuf) {
+      /* Flush all pending data */
+      if (evbuffer_get_length(bufferevent_get_output(evbuf))) {
+	printf("what sholud I do ...just disable it...\n");
+	
+	// bufferevent_setcb(evbuf,
+	// 		  NULL, close_on_flush,
+	// 		  event_func, NULL);
+	bufferevent_disable(evbuf, EV_READ|EV_WRITE);
+      } else {
+	/* We have nothing left */	
+	// bufferevent_free(evbuf);
 	return;
       }
-
-      switch (buffer[1]) { /* handle socks commands */
-      case CONNECT:
-      	handle_connect(bev, buffer, evsize);
-      	break;
-      case BIND:
-      	puts("bind");
-      	handle_bind(buffer, evsize);
-      	break;
-      case UDPASSOC:
-      	puts("udp associate");
-      	handle_udpassoc();
-      	break;
-      }
-  }
-
-  if (!partner)
-    {
-    puts("drain");
-    evbuffer_drain(src, len);
-    return;
     }
-  
-  bufferevent_free(bev);
+    bufferevent_free(bev);
+  }
+}
+
+static void
+accept_func(struct evconnlistener *listener,
+	    evutil_socket_t fd,
+	    struct sockaddr *a, int slen, void *p)
+{
+  struct bufferevent *src, *dst;
+  src = bufferevent_socket_new(base, fd,
+			       BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS);
+  dst = bufferevent_socket_new(base, fd,
+			       BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS);  
+
+  /* assert(src && dst); */
+  assert(src);
+  bufferevent_setcb(src, read_func, NULL, event_func, dst);
+  bufferevent_setcb(dst, read_func, NULL, event_func, src);
+  bufferevent_enable(src, EV_READ|EV_WRITE);
+  bufferevent_enable(dst, EV_READ|EV_WRITE);  
 }
 
 struct addrspec *
 handle_addrspec(unsigned char * buffer)
 {
   struct addrspec *spec;
-
-  uint8_t atype = buffer[3];
-  int buflen;
-  uint32_t v4addr;
-  
-  unsigned char  ip4[4];
-  unsigned char ip6[16];
-  
+  unsigned char atype = buffer[3];
+  int buflen, domlen;  
   unsigned long s_addr;
+  unsigned char ip4[4];
+  uint32_t ipv4;
+  unsigned char pb[2]; /* buffer for port */
+  unsigned short port;
+  spec = malloc(sizeof(struct addrspec));
 
-  char *domain;
-  int domlen;
-  unsigned char port[2]; /* two bytes for port */
-  uint16_t _port;  
-
-  spec = (struct addrspec*)calloc(1, sizeof(spec)); /* allocate memory */
-  if (!spec)
-    fprintf(stderr, "[ERROR: Insufficient memory.\\n");
-  
-  switch(atype) {
+  switch (atype) {
   case IPV4:
     buflen = 8;
-    /* memcpy(ip4, buffer+4, 4); */
-    memcpy(ip4, buffer+4, sizeof(ip4));
-    v4addr = (uint32_t)ip4[0] << 24|
-           (uint32_t)ip4[1] << 16|
-           (uint32_t)ip4[2] << 8|
-           (uint32_t)ip4[3];
-    s_addr = htonl(v4addr);
-    
+    memcpy(ip4, buffer+4, 4);
+    ipv4 =   (uint32_t)ip4[0] << 24|
+             (uint32_t)ip4[1] << 16|
+             (uint32_t)ip4[2] << 8|
+             (uint32_t)ip4[3];
+    s_addr = htonl(ipv4);
     (*spec).s_addr = s_addr;
-    
+    (*spec).sin_family = AF_INET;
     break;
   case IPV6:
     buflen = 20;
-    memcpy(&ip6, buffer+4, sizeof(ip6));
-    
-    if (((*spec)._s6_addr = (unsigned char*)calloc(1, sizeof(ip6))) == NULL)
-      return NULL;
-    
-    (*spec)._s6_addr = ip6;
-
+    (*spec)._s6_addr = (unsigned char*)malloc(16);
+    (*spec)._s6_addr = buffer + 4; /* jump to 16 bytes address */
+    (*spec).sin_family = AF_INET6;
+    printf("[INFO: v6=%d]\n", (*spec)._s6_addr[0]);
     break;
   case _DOMAINNAME:
     domlen = buffer[4];
     buflen = domlen + 5;
-    
-    if ((domain = (char*)calloc(1, sizeof(char)*buflen)) == NULL)
-      return NULL;
-	
-    memcpy(domain, buffer+5, sizeof(char) * domlen+1);
-    (*spec).domain = domain;    
-
+    (*spec).domain = (unsigned char*)malloc(domlen);
+    (*spec).domain = buffer + 5;
+    (*spec).sin_family = 3;
+    printf("[INFO: domain=%s len=%d]\n", (*spec).domain, domlen);
     break;
   default:
-    fprintf(stderr, "[ERROR: unknow command: %d]\n", atype);
+    fprintf(stderr, "[ERROR handle_addrspec.switch Unknown atype]\n");
     return NULL;
   }
-  
-  memcpy(port, buffer+buflen, sizeof(unsigned char)*2); /* allocation for port */
-  _port = port[0]<<8 | port[1];
-  (*spec).port = _port;
+
+  memcpy(&pb, buffer+buflen, sizeof(pb));
+  port = pb[0]<<8|pb[1];
+  (*spec).port = port;
   return spec;
 }
 
-static void
-handle_connect(struct bufferevent *bev, unsigned char *buffer, ev_ssize_t evsize)
+void
+handle_connect(struct bufferevent *bev, unsigned char *buffer, ev_ssize_t esize)
 {
-  struct addrspec *spec;
-
-  /* ip4 and ip6 are for presentation */
+  struct addrspec *spec = malloc(sizeof(struct addrspec));
+  // /* ip4 and ip6 are for presentation */
   char ip4[INET_ADDRSTRLEN];
   char ip6[INET6_ADDRSTRLEN];
   
-  if ((spec = (struct addrspec*)calloc(1, sizeof(struct addrspec))) == NULL)
-    return;
+  int i;
+  size_t len;
   
-  for (unsigned char i = 0; i < evsize; ++i) {
-    printf("%d ", buffer[i]);	
-  }
-  puts(" ");
-  
-  if ((spec = handle_addrspec(buffer)) == NULL) {
-    puts("NULL!");
-    return;
-  }
-
-  if
-    (((*spec).domain == NULL))
-    {
-      if (!(*spec).s_addr) { /* print out ip6 */
-	inet_ntop(AF_INET6, &((*spec)._s6_addr), ip6, INET6_ADDRSTRLEN);
-	printf("[INFO: %s:%d]\n", ip6, (*spec).port);
-      }
-      else { /* print out ip4 */
-	inet_ntop(AF_INET, &((*spec).s_addr), ip4, INET_ADDRSTRLEN);
-	printf("[INFO: %s:%d]\n", ip4, (*spec).port);
-      }
-    } else /* print out domain */
-    printf("[INFO: %s:%d]\n", (*spec).domain, (*spec).port);
-  
-  send_reply(bev, SUCCESSED);
-}
-
-static void
-handle_bind(unsigned char *buffer, ev_ssize_t evsize)
-{
-  for (unsigned char i = 0; i < evsize; i++) {
-    printf("%d ", buffer[i]);	
-  }
-  puts(" ");
-
-  send_reply(NULL, SUCCESSED);
-}
-
-static void
-handle_udpassoc(void)
-{
-  send_reply(NULL, (uint8_t) SUCCESSED);
-}
-
-static void
-send_reply(struct bufferevent *bev, uint8_t reply)
-{
-  const char *response;
-  size_t res_size;
-  
-  res_size = sizeof(get_socks_header());
-  
-  if ((response = (char*)calloc(1, sizeof(char)* 16)) == NULL)
-    return;
- 
-  response = get_socks_header();
-  
-  printf("gonna send this: %s\n", response);
-  
-  // response[1] = reply; some kind of copy??? from response
-  puts("send_reply");
-  if ( bev == NULL) {
-    puts("bev is null");
-    return;
-  }
-  bufferevent_write(bev, response, res_size);
-}
-
-static const char *
-get_socks_header(void)
-{
-  char *buffer;  
-  uint8_t socks_version = 5;
-
-  if ((buffer = (char*)calloc(1, sizeof(uint8_t)*16)) == NULL) /* size is estimated.. */
-    return NULL;
-  
-  socks_version = (uint8_t) 5;
-  
-  buffer[0] = socks_version;
-  buffer[1] = (uint8_t) 0; /* reply */
-  buffer[2] = (uint8_t) 0; /* reserved */  
-  buffer[3] = (uint8_t) 1; /* address type */
-  
-  buffer[4] = (uint8_t) 0; /* bind address */
-  buffer[5] = (uint8_t) 0; /* bind address */
-  buffer[6] = (uint8_t) 0; /* bind address */
-  buffer[7] = (uint8_t) 0; /* bind address */
-  
-  buffer[7] = (uint8_t) 0; /* bind port */
-  buffer[8] = (uint8_t) 0; /* bind port */
-  return buffer;
-}
-
-static void
-event_func(struct bufferevent *bev, short what, void *ctx)
-{
-  struct bufferevent *src = ctx;
-  
-  if (what & (BEV_EVENT_READING|BEV_EVENT_ERROR|BEV_EVENT_WRITING)) {
-    if (what & BEV_EVENT_ERROR) {
-      puts("BEV_EVENT_ERROR");
-      if (errno)
-	perror("connection error");
-    }
-    if (what & BEV_EVENT_READING) { /* send reply message to clinets here */
-      puts("reading now");
-      /* invoke new events here */
-    }
-    if (what & BEV_EVENT_WRITING) {
-      puts("I'm writing sth...");
-    }
+  struct evbuffer *src;
+  src = bufferevent_get_input(bev);
+  len = evbuffer_get_length(src);
     
-    if (src) {
-      reader_func(bev, ctx);
-    }
+  printf("\n[INFO: ");
+  for (i = 0; i < esize; ++i) {
+    printf("%d ", buffer[i]);	
+  }
+  puts("]");
+
+  if (esize <=4 )
+    return; /* nothing to get from this buffer */
+  
+  spec = handle_addrspec(buffer);
+  if ((spec == NULL)) {
+    return;
   }
 
-  bufferevent_free(bev); 
-}
-
-static void
-listener_func(struct evconnlistener *listener, evutil_socket_t fd,
-	    struct sockaddr *sa, int socklen, void *user_data)
-{
-  struct event_base *base = user_data;
-  struct bufferevent *bev;
-  // struct evbuffer *evb;
-
-  char serveraddr[INET_ADDRSTRLEN] = "127.0.0.1";   /* server address */
-  const char *header; /* socks5 header this is an estimated size */
-  size_t bufsize;
-
-  struct sockaddr_in ss;
-  inet_pton(AF_INET, serveraddr, &(ss.sin_addr));
-
-  if ((header = calloc(1, sizeof(char)*7)) == NULL)
-    return;
-  
-  header = get_socks_header();
-  bufsize = sizeof(header);
-  
-  bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
-  if (!bev)
-    error_exit("bufferevent_socket_new");
-
-  bufferevent_setcb(bev, reader_func, NULL, event_func, NULL);
-  if (bufferevent_enable(bev, EV_READ) != 0 )
-    error_exit("bufferevet_enable");
-  bufferevent_write(bev, header, bufsize);
+  /* going to present address */
+  switch ((*spec).sin_family) {
+  case AF_INET:
+    if (!((inet_ntop(AF_INET, &((*spec).s_addr), ip4, INET_ADDRSTRLEN)) == NULL)) {
+      printf("[INFO: v4=%s:%d]\n", ip4, (*spec).port);
+    }
+    break;
+  case AF_INET6:
+    if (!((inet_ntop(AF_INET6, &((*spec)._s6_addr), ip6, INET6_ADDRSTRLEN)) == NULL)) {
+      printf("[INFO: v6=%s:%d]\n", ip6, (*spec).port);
+    }
+    break;
+  case 3:
+    printf("[INFO: domain=%s:%d]\n", (*spec).domain, (*spec).port);
+    break;
+  default:
+    fprintf(stderr, "[ERROR: Unknow family]\n");
+    break;
+  }
+  /* drain a read buffer */
+  evbuffer_drain(src, len);
 }
 
 static void
 syntax(void)
 {
-  printf("evsocks [--help] [-v] [-h] [--host] [-p] [--port] <listen-on-addr> <port>\n");
+  printf("evsocks [--help] [-v] [-h] [-p]\n");
   exit(EXIT_SUCCESS);
 }
 
@@ -336,97 +337,76 @@ static int verbose = 0;
 int
 main(int argc, char **argv)
 {
-  int i, p;
-  int has_host = 0;
-  int has_port = 0;
-  int socklen;
-  static struct sockaddr_storage listen_on_addr;
-  struct event_base *base;
+  struct options {
+    const char *port;
+    const char *host;
+  };
+  struct options o;
+  char opt;
+
+  int socklen, port;  
   struct evconnlistener *listener;  
-  /* mode_t fperms = S_IRUSR|S_IWUSR; */
-  
-  if (argc < 3)
+  static struct sockaddr_storage listen_on_addr;
+
+  memset(&o, 0, sizeof(o));
+
+  if (argc < 2) {
     syntax();
-
-  for ( i = 1; i < argc; i++) {
-
-    if (strcmp(argv[i], "-v") == 0) {
-      printf("verbose+");
-      verbose++;
-    }
-
-    if (strcmp(argv[i], "-h") == 0) {
-      printf("host: %s\n", argv[i+1]);
-      has_host = i+1;
-    }
-    else if (strcmp(argv[i], "--host") == 0) {
-      printf("host: %s\n", argv[i+1]);      
-      has_host = i+1;
-    }
-    
-    if (strcmp(argv[i], "-p") == 0) {
-      printf("port : %s\n", argv[i+1]);
-      has_port = i+1;      
-    }
-    else if (strcmp(argv[i], "--port") == 0)
-      has_port = i+1;
-    
-    if (strcmp(argv[i], "--help") == 0)
-      syntax();
   }
 
- /* TODO: let users chagne host */
-  fprintf(stderr, "Server is up and running 0.0.0.0:%s\n", argv[has_port]);
-  
+  while ((opt = getopt(argc, argv, "vh:p:")) != -1) {
+    switch (opt) {
+    case 'v': ++verbose; break;
+    case 'h': o.host = optarg; break;      
+    case 'p': o.port = optarg; break;
+    default: fprintf(stderr, "Unknow option %c\n", opt); break;      
+    }
+  }
+
+  if (o.host == NULL) {
+    syntax();
+  }
+  if (o.port == NULL) {
+    syntax();
+  }
+
+  /* get started */
   memset(&listen_on_addr, 0, sizeof(listen_on_addr));
   socklen = sizeof(listen_on_addr);
-  if (evutil_parse_sockaddr_port(argv[has_host],
-				 (struct sockaddr*)&listen_on_addr, &socklen)<0) {
-    p = atoi(argv[has_port]);
-    struct sockaddr_in *sin = (struct sockaddr_in *)&listen_on_addr;
-    if (p < 1 || p > 65535)
-      syntax();
-    (*sin).sin_port = htons(p);
-    (*sin).sin_addr.s_addr = htonl(0x7f000001);
-    (*sin).sin_family = AF_INET;
-    socklen = sizeof(struct sockaddr_in);
-  }
   
-#ifdef _WIN32
-  WSADATA wsa_data;
-  WSAStartup(0x0201, &wsa_data);  
-#endif
+  if (evutil_parse_sockaddr_port(o.port, (struct sockaddr*)&listen_on_addr, &socklen)<0) {
+      struct sockaddr_in *sin = (struct sockaddr_in*)&listen_on_addr;
+      port = atoi(o.port);
+      if (port < 1 || port > 65535)
+	syntax();
+      (*sin).sin_port = htons(port);
+      if (inet_pton(AF_INET, o.host, &((*sin).sin_addr)) == -1)
+	syntax();
+      (*sin).sin_family = AF_INET;
+      socklen = sizeof(struct sockaddr_in);
+    }
 
   base = event_base_new();
-  if (!base)
-    error_exit("event_base_new()");
+  if (!base) {
+    perror("event_base_new()");
+    return 1;
+  }
 
-  listener = evconnlistener_new_bind(base, listener_func, (void *)base,
-				     LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE, -1,
-				     (struct sockaddr*)&listen_on_addr,
-				     socklen);
+  listener = evconnlistener_new_bind(base, accept_func, NULL,
+				     LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE|LEV_OPT_CLOSE_ON_EXEC,
+				     -1, (struct sockaddr*)&listen_on_addr, socklen);
+  if (!listener) {
+    perror("evconnlistener_new_bind()");
+    event_base_free(base);
+    return 1;
+  }
   
-  if (!listener)
-    error_exit("evconlistner_new_bind");
+  printf(" [INFO Server is up and running: %s:%s]\n", o.host, o.port);
+  printf(" [INOF level=%d]\n", verbose);
   
-  /* TODO: Add singal to stop a server */
   event_base_dispatch(base);
   evconnlistener_free(listener);
   event_base_free(base);
-  printf("done\n");
+
   exit(EXIT_SUCCESS);
-}
-
-
-static const char *
-ntov4a(uint32_t address)
-{
-  static char buf[32];
-  uint32_t a = ntohl(address);
-  evutil_snprintf(buf, sizeof(buf), "%d.%d.%d.%d",
-		  (int)(uint8_t)((a>>24)&0xff),
-		  (int)(uint8_t)((a>>16)&0xff),
-		  (int)(uint8_t)((a>>8 )&0xff),
-		  (int)(uint8_t)((a	)&0xff));
-  return buf;
-}
+}  
