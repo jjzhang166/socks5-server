89d578a236faaba7ecd290298ff898b3c5ca3fd7
xun <lu.xun@gmx.com>
2017-11-27 20:10:33 +0000

    Clean up a bit and care about illegal requests
    I did a bit of refactoring. I also added new function
    that will invoke signal_event and the application gracefully exits.
    When illegal requests come in, the bufferevent will be removed without
    any mess.
    
    TODO:
      ssl/tls support
      resolve domain using getaddrinfo or libevent dns api??

--- src/evsocks.c
+++ src/evsocks.c
@@ -31,7 +31,6 @@
 
 #include <signal.h>
 #include <sys/stat.h>
-#include <fcntl.h>
 
 #include <event2/bufferevent.h>
 #include <event2/buffer.h>
@@ -45,7 +44,6 @@
 static struct event_base *base;
 
 static int status;
-
 
 static void
 handle_perpetrators(struct bufferevent *bev)
@@ -62,10 +60,9 @@ syntax(void)
   exit(EXIT_SUCCESS);
 }
 
-void
+static void
 event_func(struct bufferevent *bev, short what, void *ctx)
 {
-
   struct bufferevent *associate = ctx;
   
   if (what & (BEV_EVENT_EOF|BEV_EVENT_CONNECTED|BEV_EVENT_ERROR)) {
@@ -80,7 +77,7 @@ event_func(struct bufferevent *bev, short what, void *ctx)
       bufferevent_free(bev);
       if (associate)
 	bufferevent_free(associate);
-      puts("** freed");
+      puts("* freed");
     }
 
     if (what & BEV_EVENT_CONNECTED) {
@@ -88,26 +85,11 @@ event_func(struct bufferevent *bev, short what, void *ctx)
       /* ready for next event */
       bufferevent_setcb(bev,
 			async_read_from_target_func,
-			on_write_and_drain, event_func, associate);
+			NULL, event_func, associate);
     }
   }
 }
 
-static void
-on_write_and_drain(struct bufferevent *bev, void *ctx)
-{
-  struct evbuffer *evb = bufferevent_get_output(ctx);
-  ev_ssize_t evsize = evbuffer_get_length(evb);
-  
-  puts("* on_write_and_drain");
-  if (evsize > 0) { /* avoid leaving early */
-    // bufferevent_free(bev);
-    evbuffer_drain(evb, evsize);
-    status = SWRITE;
-  }
-}
-
-/* listens to target address and write data back to clietns */
 static void
 async_write_func(struct bufferevent *bev, void *ctx)
 {
@@ -123,8 +105,8 @@ async_write_func(struct bufferevent *bev, void *ctx)
 
   /* handle leftover here */
   switch (status) {
-  case SREAD:    
-    puts("* SREAD");
+  case SREAD:
+    printf("** async_write_func drains %ld\n", buf_size);
     evbuffer_drain(src, buf_size);
     bufferevent_enable(associate, EV_READ);
     break;
@@ -142,24 +124,30 @@ async_write_func(struct bufferevent *bev, void *ctx)
 static void
 async_read_func(struct bufferevent *bev, void *ctx)
 {
-  struct bufferevent *associate = ctx; /* we will have a talk with an associate */
+  /* we will have a talk with our associate */
+  struct bufferevent *associate = ctx;
   struct evbuffer *src;
-  static struct addrspec *spec; /* this will hold spec til my associate is ready to talk */
-  ev_ssize_t evsize; /* we will store buffer size here */
-  size_t buf_size; /* how many bytes read so far? */
-  unsigned char payload[12] = {5, 0, 5, 0, 0, 1, 0, 0, 0, 0, 0, 0}; /* ready for socks payload */
-  unsigned char failed[10];
-  
+  /* keep spec */
+  static struct addrspec *spec;
+  /* store copied buffer size here */
+  ev_ssize_t evsize;
+  /* store size from bev */
+  size_t buf_size;
+
   src = bufferevent_get_input(bev);
   buf_size = evbuffer_get_length(src);
-    
+  
+  /* socks payload */
+  unsigned char payload[12] = {5, 0, 5, 0, 0, 1, 0, 0, 0, 0, 0, 0};
+  /* in case, failed for some reasons */
+  unsigned char failed[10];
+  
   unsigned char header[buf_size];
   evsize = evbuffer_copyout(src, header, buf_size);
 
   if (header[0] == SOCKS_VERSION) {
     
     puts("* initialize service");
-    
     /* start to parse data */
     switch (header[1]) {
 
@@ -167,13 +155,17 @@ async_read_func(struct bufferevent *bev, void *ctx)
       evbuffer_drain(src, 3); /* drain 3 bytes */
       spec = handle_connect(bev, header, evsize);      
       break;
+      
     case BIND:
       evbuffer_drain(src, 4); /* drain 4 bytes */
       spec = handle_connect(bev, header, evsize);            
       break;
+      
     case UDPASSOC:
       puts("* udp associate not supported");
+      status = SDESTORY;
       break;
+      
     default:
       fprintf(stderr, "** unknown command");
       status = SDESTORY;
@@ -184,56 +176,47 @@ async_read_func(struct bufferevent *bev, void *ctx)
       fprintf(stderr, "** async_read_func._write");
       status = SDESTORY;
     }
-    
-    /* make sure we have a spec and then send the rest of buffer */
+
+    /* make sure we have a spec and then can send the rest of buffer */
     if (spec) {
-      // status = SWAIT;
+      
+      debug_addr(spec); /* debug address */
+      
       if (bufferevent_write(bev, payload+4, 8)<0) {
       	fprintf(stderr, "** async_read_func._write");
       	status = SDESTORY;
       }
-    } else {
-      /* TODO                                            */
-      /*   send socks failure reply in case spec is NULL */        
     }
     
     /* wait til buffer size is enough to talk to target */
     if (evsize <= 4) {
-      
       status = SWAIT;
-      return; /* return nad wait for next event */
-      
-    } else if (status == SREAD) {
-      
-      puts("* don't disturb I'm readling..");
-      
+      return; /* return and wait for next event */
     } else {
-      
       status = SWRITE;
     }
 
   } else if (associate && status == SWRITE) {
-    
+
     puts("* Our associate is ready for work");
 
     unsigned char *reqbuf;
     reqbuf = (unsigned char*)malloc(evsize);
     
     reqbuf = header;
-    
+
     printf("* payload=%ld\n", evsize);
 
     bufferevent_setcb(associate, NULL, NULL, event_func, bev); /* set up callbacks */
     bufferevent_enable(associate, EV_READ);
 
     struct sockaddr_in target;
+    
     memset(&target, 0, sizeof(target));
     
     target.sin_family = AF_INET;       
     target.sin_addr.s_addr = (*spec).s_addr;
     target.sin_port = htons((*spec).port);
-
-    debug_addr(spec); /* debug address */
 
     if (bufferevent_socket_connect(associate,
 				   (struct sockaddr*)&target, sizeof(target))<0){
@@ -245,34 +228,20 @@ async_read_func(struct bufferevent *bev, void *ctx)
       perror("_write");
       status = SDESTORY;
     }
-    
-    /* write out date to target  */
-    if (bufferevent_write(associate, reqbuf, evsize)<0){
-      
-      status = SDESTORY;
-      
+
+    /* write out data to target  */
+    if (bufferevent_write(associate, reqbuf, evsize)<0){      
+      status = SDESTORY;      
     } else if (status == SDESTORY) {
       
-      handle_perpetrators(associate);    
-    }    
+      handle_perpetrators(associate);  
+    }
   } else {
     /* this buffer must be purged */
     puts("** this client seems wrong");
+    handle_perpetrators(bev);
   }
 }
-
-// static void
-// watch_read(struct bufferevent *bev, void *ctx)
-// {
-//   struct evbuffer *evb = bufferevent_get_output(bev);
-//   ev_ssize_t evsize = evbuffer_get_length(evb);  
-//   
-//   puts("* watch_read");
-//   if (evsize > 0) { /* avoid leaving early */
-//     printf("* have some buffer %d\n", evsize);
-//   }
-// }
-//
 
 static void
 async_read_from_target_func(struct bufferevent *bev, void *ctx)
@@ -281,7 +250,7 @@ async_read_from_target_func(struct bufferevent *bev, void *ctx)
   struct evbuffer *src;
   ev_ssize_t evsize; /* we will store buffer size here */
   size_t buf_size; /* how many bytes read so far? */
-  
+
   src = bufferevent_get_input(bev);
   buf_size = evbuffer_get_length(src);
     
@@ -291,16 +260,22 @@ async_read_from_target_func(struct bufferevent *bev, void *ctx)
   printf("* says=%ld bytes\n", evsize);
 
   if (status == SWRITE) {
-    /* disable once and then enable it again when job done */
-    puts("* disable");
+    /* disable once and then enable it again when status is SREAD */
     bufferevent_disable(bev, EV_READ);
   }
-  
-  else if (status == SREAD) {
-    puts("*** what now");
-    // bufferevent_enable(bev, EV_READ);
-  }
 
+  else if (status == SREAD) {
+    /* drain buffer from the other side */
+    evbuffer_drain(src, buf_size);
+  }
+  
+  /* 
+     make sure status is set to SREAD. 
+     Otherwise, clients will leave early.
+     
+   */
+  
+  puts("** set to SREAD");
   status = SREAD;
   
   if (bufferevent_write(associate, buffer, evsize)<0) {
@@ -315,39 +290,57 @@ accept_func(struct evconnlistener *listener,
 	    evutil_socket_t fd,
 	    struct sockaddr *a, int slen, void *p)
 {
+  /* 
+     Both src and dst will have a talk over an evbffer.
+ */
   struct bufferevent *src, *dst;
   src = bufferevent_socket_new(base, fd,
 			       BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS);
   
-  /* note: dst's fd should be -1 since we do not want it to connect some address yet */
+  /* note: dst's fd should be -1 since we do not want it to connect to target yet */
   dst = bufferevent_socket_new(base, -1, 
-			       BEV_OPT_CLOSE_ON_FREE); //|BEV_OPT_DEFER_CALLBACKS);
+			       BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS);
+  
+  assert(src && dst);
   
   bufferevent_setcb(src, async_read_func, async_write_func, event_func, dst);
   bufferevent_enable(src, EV_READ);
 }
 
+static void
+signal_func(evutil_socket_t sig_flag, short what, void *ctx)
+{
+  struct event_base *base = ctx;
+  struct timeval delay = {1, 0};
+  int sec = 1;
+  
+  printf("*** Caught an interupt signal; exiting cleanly in %d second(s)\n", sec);
+  event_base_loopexit(base, &delay);
+}
+
 int
 main(int argc, char **argv)
 {
-  struct options {
+    struct options {
     const char *port;
     const char *host;
   };
   struct options o;
   char opt;
   static int verbose;
+  
   int socklen, port;  
   struct evconnlistener *listener;  
   static struct sockaddr_storage listen_on_addr;
+  struct event *signal_event;
 
-  memset(&o, 0, sizeof(o));
-
-  if (argc < 2) {
+  if (argc < 2 || (strcmp(argv[1], "--help") == 0)) {
     syntax();
   }
 
-  while ((opt = getopt(argc, argv, "vhp:uqcs")) != -1) {
+  memset(&o, 0, sizeof(o));
+
+  while ((opt = getopt(argc, argv, "h:p:vuqcs")) != -1) {
     switch (opt) {
     case 'v': ++verbose; break;
     case 'h': o.host = optarg; break;
@@ -356,10 +349,10 @@ main(int argc, char **argv)
     }
   }
 
-  if (o.host == NULL) {
+  if (!o.host) {
     o.host = "0.0.0.0";
   }
-  if (o.port == NULL) {
+  if (!o.port) {
     syntax();
   }
 
@@ -382,20 +375,27 @@ main(int argc, char **argv)
   base = event_base_new();
   if (!base) {
     perror("event_base_new()");
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   listener = evconnlistener_new_bind(base, accept_func, NULL,
-				     LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE|LEV_OPT_CLOSE_ON_EXEC,
+		     LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE|LEV_OPT_CLOSE_ON_EXEC,
 				     -1, (struct sockaddr*)&listen_on_addr, socklen);
   if (!listener) {
     perror("evconnlistener_new_bind()");
     event_base_free(base);
-    return 1;
+    exit(EXIT_FAILURE);    
   }
-
+  
   printf("\n* %s:%s *\n", o.host, o.port);
   printf("* level=%d\n", verbose);
+
+  signal_event = event_new(base, SIGINT, EV_SIGNAL|EV_PERSIST, signal_func, (void*)base);
+
+  if (!signal_event || event_add(signal_event, NULL)) {
+    fprintf(stderr, "** Cannot add a signal_event\n");
+    exit(EXIT_FAILURE);
+  }
 
   event_base_dispatch(base);
   event_base_free(base);
