bd9283f65831a1aa5438e717cafeac2a5159b6f0
xun <lu.xun@gmx.com>
2018-01-07 15:21:13 +0900

    Add getopt-long
    Options are find to use now. I'm working on
    local and serer mode functions right now.

--- evs_server.c
+++ evs_server.c
@@ -1,10 +1,12 @@
 /* 
+ * server.c
+ *
  * Use of this source code is governed by a
  * license that can be found in the LICENSE file.
  *
  * Simple proxy server with Libevent 
  * 
-*/
+ */
 
 #if defined(__APPLE__) && defined(__clang__)
 #program clang diagnostic ignored "-Wdeprecated-declarations"
@@ -15,6 +17,7 @@
 #include <ws2tcip.h>
 #else
 
+#include <getopt.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -29,6 +32,9 @@
 #include "evs_log.h"
 
 #define MAX_OUTPUT (512 * 1024)
+
+/* local flag */
+static int yes_this_is_local;
 
 const char *auth = NULL;
 
@@ -55,17 +61,15 @@ static void signal_func(evutil_socket_t sig_flag, short what, void *ctx);
 static void
 syntax(void)
 {
-  printf("USAGE: esocks [-p port] [options...]\n");
-  printf("\n");
+  printf("Usage: esocks [options]\n");
   printf("Options:\n");
-  printf("  -c client mode\n");
-  printf("  -s server mode\n");
-  printf("  -k password\n");
-  printf("  -d daemon\n");
-  printf("  -p port\n");
-  printf("  -h host\n");
-  printf("  -d debug mode\n");
-  printf("\n");  
+  printf(" -g --server_addr\n");
+  printf(" -h --server_port\n");  
+  printf(" -i --local_addr\n");
+  printf(" -j --local_port\n");
+  printf(" -k --password\n");
+  printf(" -v --verbose\n");
+  printf("\n");
   exit(EXIT_SUCCESS);
 }
 
@@ -76,7 +80,7 @@ signal_func(evutil_socket_t sig_flag, short what, void *ctx)
   struct timeval delay = {1, 0};
   int sec = 1;
   
-  logger_err("Caught an interupt signal; exiting cleanly in %d second(s)", sec);
+  logger_info("Caught an interupt signal; exiting cleanly in %d second(s)", sec);
   event_base_loopexit(base, &delay);
 }
 
@@ -96,7 +100,7 @@ close_on_finished_writecb(struct bufferevent *bev, void *ctx)
 
   if (evbuffer_get_length(evb) == 0) {
     bufferevent_free(bev);
-    logger_debug(verbose, "close_on_finished_writecb freed");    
+    logger_debug(verbose_flag, "close_on_finished_writecb freed");    
   }
 }
 
@@ -129,7 +133,7 @@ eventcb(struct bufferevent *bev, short what, void *ctx)
 	bufferevent_free(partner);
       }
     }
-    logger_debug(verbose, "freed");
+    logger_debug(verbose_flag, "freed");
     bufferevent_free(bev);
   }
 }
@@ -149,7 +153,7 @@ socks_initcb(struct bufferevent *bev, void *ctx)
   evsize = evbuffer_copyout(src, reqbuf, buf_size);
    
   if (reqbuf[0] == SOCKS_VERSION) {
-    logger_debug(verbose, "connecting");
+    logger_debug(verbose_flag, "connecting");
     status = SINIT;
     if (bufferevent_write(bev, payload, 2)<0) {
       logger_err("socks_initcb.bufferevent_write");
@@ -176,8 +180,6 @@ readcb(struct bufferevent *bev, void *ctx)
   struct evbuffer *src, *dst;
   
   static struct addrspec *spec;
-  /* struct dns_context *dnsctx; */
-  /* const char *nameservers[] = {"8.8.8.8", "8.8.4.4"}; */
   
   u8 payload[10] = {5, 0, 0, 1, 0, 0, 0, 0, 0, 0};
 
@@ -191,70 +193,84 @@ readcb(struct bufferevent *bev, void *ctx)
   evbuffer_copyout(src, buffer, buflen);
 
   /* Check if version is correct and status is equal to INIT */
-  if (status == SINIT && buffer[0] == SOCKS_VERSION) {
-
-    /* parse socks header */
-    switch (buffer[1]) {
-    case CONNECT:
-    case BIND:
-      spec = handle_addrspec(buffer);
-      if (spec == NULL) {
-	logger_debug(verbose, "BIND and CONNECT and give us back NULL spec");
-	payload[1] = HOST_UNREACHABLE;
+  if (status == SINIT && buffer[0] == SOCKS_VERSION)
+    {
+      /* parse socks header */
+      switch (buffer[1]) {
+      case CONNECT:
+      case BIND:
+	spec = handle_addrspec(buffer);
+	if (spec == NULL) {
+	  logger_debug(verbose_flag, "BIND and CONNECT and give us back NULL spec");
+	  payload[1] = HOST_UNREACHABLE;
+	}
+	break;
+      case UDPASSOC:
+	logger_warn("protocol(%d) is not supported", buffer[1]);
+	payload[1] = NOT_SUPPORTED;
+	spec = NULL;
+	break;
+      default:
+	logger_err("unknown command %d", buffer[1]);
+	payload[1] = GENERAL_FAILURE;
+	spec = NULL;
       }
-      break;
-    case UDPASSOC:
-      logger_warn("protocol(%d) is not supported", buffer[1]);
-      payload[1] = NOT_SUPPORTED;
-      spec = NULL;
-      break;
-    default:
-      logger_err("unknown command %d", buffer[1]);
-      payload[1] = GENERAL_FAILURE;
-      spec = NULL;
-    }
 
-    debug_addr(spec);
+      debug_addr(spec);
 
-    if (spec == NULL) {
-      /* spec cannot be NULL */
-      logger_info("spec is null and destroy");
-      if (bufferevent_write(bev, payload, 10)<0)
-	logger_err("bufferevent_write");
-      destroycb(bev);
-      return;
+      if (spec == NULL)
+	{
+	  /* spec cannot be NULL */
+	  logger_info("spec is null and destroy");
+	  if (bufferevent_write(bev, payload, 10)<0)
+	    logger_err("bufferevent_write");
+	  destroycb(bev);
+	  return;
       
-    } else {
-
-      bufferevent_enable(bev, EV_WRITE);      
-      /* TODO: */
-      /*    how about IPv6?? */
-      if (spec->family == AF_INET)  {
-  	struct sockaddr_in target;
-  	target.sin_family = AF_INET;
-  	target.sin_addr.s_addr = spec->s_addr;
-  	target.sin_port = htons(spec->port);
+	}
+      else
+	{
+	  if (yes_this_is_local)
+	    {
+	      logger_info("sending to server ...");
+	      /* Await partner's event */
+	      bufferevent_setcb(bev, after_connectcb, NULL, eventcb, partner);
+	      bufferevent_enable(bev, EV_WRITE|EV_READ);
+	    }
+	  else if (!yes_this_is_local)
+	    {
+	      bufferevent_enable(bev, EV_WRITE);      
+	      /* TODO: */
+	      /*    how about IPv6?? */
+	      if (spec->family == AF_INET)  {
+		struct sockaddr_in target;
+		target.sin_family = AF_INET;
+		target.sin_addr.s_addr = spec->s_addr;
+		target.sin_port = htons(spec->port);
   	
-  	free(spec);
+		free(spec);
   	
-  	if (bufferevent_socket_connect(partner,
-  				       (struct sockaddr*)&target, sizeof(target)) != 0) {	
-  	  logger_err("failed to connect");
-  	  payload[1] = HOST_UNREACHABLE;	
-  	  if (bufferevent_write(bev, payload, 10)<0) {
-  	    logger_err("bufferevent_write");
-  	  }
-  	  destroycb(bev);
-  	  return;
-  	}
+		if (bufferevent_socket_connect(partner,
+					       (struct sockaddr*)&target, sizeof(target)) != 0)
+		  {
+	    
+		    logger_err("failed to connect");
+		    payload[1] = HOST_UNREACHABLE;	
+		    if (bufferevent_write(bev, payload, 10)<0) {
+		      logger_err("bufferevent_write");
+		    }
+		    destroycb(bev);
+		    return;
+		  }
   
-  	evbuffer_drain(src, buflen);
-  	/* Await partner's event */
-  	bufferevent_setcb(bev, after_connectcb, NULL, eventcb, partner);
-  	bufferevent_enable(bev, EV_WRITE|EV_READ);
-      }
+		evbuffer_drain(src, buflen);
+		/* Await partner's event */
+		bufferevent_setcb(bev, after_connectcb, NULL, eventcb, partner);
+		bufferevent_enable(bev, EV_WRITE|EV_READ);
+	      }
+	    }
+	}
     }
-  }
 }
   
 static void
@@ -293,7 +309,7 @@ readcb_from_target(struct bufferevent *bev, void *ctx)
   /* partner is a client. */
   /* bev is a target.   */
   if (!partner) {
-    logger_debug(verbose, "readcb_from_target drain");
+    logger_debug(verbose_flag, "readcb_from_target drain");
     evbuffer_drain(src, buflen);
     return;
   }
@@ -305,7 +321,7 @@ readcb_from_target(struct bufferevent *bev, void *ctx)
   evbuffer_add_buffer(dst, src);
 
   if (evbuffer_get_length(dst) >= MAX_OUTPUT) {
-    logger_debug(verbose, "exceeding MAX_OUTPUT %ld", evbuffer_get_length(dst));
+    logger_debug(verbose_flag, "exceeding MAX_OUTPUT %ld", evbuffer_get_length(dst));
     bufferevent_setcb(partner, NULL, drained_writecb, eventcb, bev);
     bufferevent_setwatermark(partner, EV_WRITE, MAX_OUTPUT/2,
  			     MAX_OUTPUT);
@@ -317,7 +333,7 @@ static void
 drained_writecb(struct bufferevent *bev, void *ctx)
 {
   struct bufferevent *partner = ctx;
-  logger_debug(verbose, "EXCEEDING MAX_OUTPUT %ld",
+  logger_debug(verbose_flag, "EXCEEDING MAX_OUTPUT %ld",
 	       evbuffer_get_length(bufferevent_get_output(bev)));  
   bufferevent_setcb(partner, readcb_from_target,
 		    NULL, eventcb, bev);
@@ -342,114 +358,192 @@ writecb(struct bufferevent *bev, void *ctx)
 
 static void
 acceptcb(struct evconnlistener *listener,
-	    evutil_socket_t fd,
-	    struct sockaddr *a, int slen, void *p)
+	 evutil_socket_t fd,
+	 struct sockaddr *a, int slen, void *p)
 {
-  struct bufferevent *src, *dst;
+  struct sockaddr *sa = p;
+  struct bufferevent *bev, *partner;
 
   /* BEV_OPT_CLOSE_ON_FREE 
    *  close the underlying socket, free an underlying bufferevent
    * BEV_OPT_DEFER_CALLBACKS 
    *  Deffer callbacks
- */
-  src = bufferevent_socket_new(base, fd,
+   */
+  bev = bufferevent_socket_new(base, fd,
 			       BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS);
-  /* fd should be -1 here since we have no fd whatsoever */   
-  dst = bufferevent_socket_new(base, -1, 
-			       BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS);
-  bufferevent_setcb(src, socks_initcb, NULL, eventcb, dst);
-  bufferevent_enable(src, EV_READ|EV_WRITE);
+  if (yes_this_is_local)
+    {
+      partner = bufferevent_socket_new(base, -1, 
+				   BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS);
+      if (bufferevent_socket_connect(partner, sa, sizeof(struct sockaddr_in))<0)
+	{
+	  logger_err("Obviously, forward server is down");
+	}
+    }
+  else 
+    /* fd should be -1 here since we have no fd whatsoever */    
+    partner = bufferevent_socket_new(base, -1, 
+				 BEV_OPT_CLOSE_ON_FREE|BEV_OPT_DEFER_CALLBACKS);
+
+  assert(bev && partner);
+  
+  bufferevent_setcb(bev, socks_initcb, NULL, eventcb, partner);
+  bufferevent_enable(bev, EV_READ|EV_WRITE);
 }
 
 int
-main(int argc, char **argv)
-{  
+main(int c, char **v)
+{
+
+  int cc;
   struct options {
-    const char *port;
-    const char *host;
-    const char *auth;
-    const char *nameserver;
+    const char *server_addr;    
+    const char *server_port;    
+    const char  *local_addr;
+    const char  *local_port;        
+    const char    *password;
   };
+
+  const char *servers[128];
   struct options o;
   char opt;  
-
-  int socklen, port;  
-
-  struct evconnlistener *listener;  
-  static struct sockaddr_storage listen_on_addr;
-  struct event *signal_event;
   
-  if (argc < 2 || (strcmp(argv[1], "--help") == 0)) {
-    syntax();
-  }
-
   memset(&o, 0, sizeof(o));
-
-  while ((opt = getopt(argc, argv, "h:p:adn:")) != -1) {
-    switch (opt) {
-    case 'd': ++verbose; break;
-    case 'h': o.host = optarg; break;
-    case 'p': o.port = optarg; break;
-      // case 'a': o.auth = optarg; break;
-    case 'n': o.nameserver = optarg; break;
-    default: fprintf(stderr, "Unknow option=%c\n", opt); break;      
-    }
-  }
-
-  if (!o.host) {
-    /* htonl(0x7f000001) == 127.0.0.1 */
-    o.host = "0.0.0.0";
-  }
-  if (!o.port) {
-    syntax();
-  }
   
-  /* if (o.auth) { */
-  /*   auth = calloc(strlen(o.auth), sizeof(char)); */
-  /*   auth = o.auth; */
-  /* } */
+  while (1)
+    {
+      static struct option lp[] =
+	{
+	  {"verbose_flag", no_argument, &verbose_flag, 1},
+	  {"local",        no_argument, &yes_this_is_local, 1},
+	  {"server_addr", required_argument, 0, 'g'},
+	  {"server_port", required_argument, 0, 'h'},
+	  {"local_addr",  required_argument, 0, 'i'},
+	  {"local_port",  required_argument, 0, 'j'},
+	  {"password",    required_argument, 0, 'k'}
+	};
 
-  /* allocate space for sockaddr_in */
+      int opt_index = 0;
+      
+      cc = getopt_long(c, v, "vlg:h:i:j:k:", lp, &opt_index);
+      
+      if (cc == -1)
+	break;
+
+      switch(cc)
+	{
+	case 0:
+	  if (lp[opt_index].flag != 0) break;
+	case 'g': o.server_addr = optarg; break;
+	case 'h': o.server_port = optarg; break;
+	case 'i': o.local_addr  = optarg; break;
+	case 'j': o.local_port  = optarg; break;
+	case 'k': o.password    = optarg; break;
+	case 'l':              yes_this_is_local; break;
+	case 'v':         verbose_flag++; break;
+	case '?':               syntax(); break;
+	default:                abort();
+	}
+    }
+  
+  if (!(o.server_addr||o.server_port||o.local_addr||o.local_port||o.password))
+    syntax();
+
+  int              fsocklen, socklen, port, mode;
+  struct evconnlistener               *listener;  
+  static struct sockaddr_storage listen_on_addr;
+  static struct sockaddr_storage   forward_addr;  
+  struct event                    *signal_event;
+  
   memset(&listen_on_addr, 0, sizeof(listen_on_addr));
   socklen = sizeof(listen_on_addr);
-  
-  if (evutil_parse_sockaddr_port(o.port, (struct sockaddr*)&listen_on_addr, &socklen)<0) {
-    struct sockaddr_in *sin = (struct sockaddr_in*)&listen_on_addr;
-    port = atoi(o.port);
-    if (port < 1 || port > 65535)
-      syntax();
-    sin->sin_port = htons(port);
-    if (evutil_inet_pton(AF_INET, o.host, &(sin->sin_addr))<0)
-      syntax();
-    sin->sin_family = AF_INET;
-    socklen = sizeof(struct sockaddr_in);
-  }
 
+  memset(&forward_addr, 0, sizeof(forward_addr));
+  fsocklen = sizeof(forward_addr);
+  
   base = event_base_new();
   
-  /* evdns_base = evdns_base_new(base, EVDNS_BASE_DISABLE_WHEN_INACTIVE);  */
-  
-  listener = evconnlistener_new_bind(base, acceptcb, NULL,
-		     LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE|LEV_OPT_CLOSE_ON_EXEC,
-				     -1, (struct sockaddr*)&listen_on_addr, socklen);
+  if (yes_this_is_local)
+    /* Requested running in local mode
+       Server should connect to a remote server
+    */
+    {
+
+      if (evutil_parse_sockaddr_port(o.local_port, (struct sockaddr*)&listen_on_addr, &socklen)<0)
+	{
+	  struct sockaddr_in *sin = (struct sockaddr_in*)&listen_on_addr;
+	  port = atoi(o.local_port);
+	  if (port < 1 || port > 65535)
+	    syntax();
+	  sin->sin_port = htons(port);
+	  if (evutil_inet_pton(AF_INET, o.local_addr, &sin->sin_addr)<0)
+	    syntax();
+	  sin->sin_family = AF_INET; /* TODO IPv6 */
+	  socklen = sizeof(struct sockaddr_in);
+	}
+      
+      /* prep forward server */
+      if (evutil_parse_sockaddr_port(o.server_port, (struct sockaddr*)&forward_addr, &socklen)<0)
+	{
+	  struct sockaddr_in *fsin = (struct sockaddr_in*)&forward_addr;
+	  port = atoi(o.server_port);
+	  if (port < 1 || port > 65535)
+	    syntax();
+	  fsin->sin_port = htons(port);
+	  if (evutil_inet_pton(AF_INET, o.server_addr, &fsin->sin_addr)<0)
+	    syntax();
+	  fsin->sin_family = AF_INET; /* TODO IPv6 */
+	  fsocklen = sizeof(struct sockaddr_in);
+	}
+      
+      listener = evconnlistener_new_bind(base, acceptcb, (struct sockaddr*)&forward_addr,
+					 LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE|LEV_OPT_CLOSE_ON_EXEC,
+					 -1, (struct sockaddr*)&listen_on_addr, socklen);
+      logger_info("server is up and running %s:%s connecting %s:%s",
+		  o.local_addr, o.local_port, o.server_addr, o.server_port);      
+    }
+  else
+    {
+      /* Running as forward server */
+      if (evutil_parse_sockaddr_port(o.server_port, (struct sockaddr*)&listen_on_addr, &socklen)<0)
+	{
+	  struct sockaddr_in *sin = (struct sockaddr_in*)&listen_on_addr;
+	  port = atoi(o.server_port);
+	  if (port < 1 || port > 65535)
+	    syntax();
+	  sin->sin_port = htons(port);
+	  if (evutil_inet_pton(AF_INET, o.server_addr, &sin->sin_addr)<0)
+	    syntax();
+	  sin->sin_family = AF_INET; /* TODO IPv6 */
+	  socklen = sizeof(struct sockaddr_in);
+	}
+      
+      /* Ready for forward connections from clients */
+      listener = evconnlistener_new_bind(base, acceptcb, NULL,
+					 LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE|LEV_OPT_CLOSE_ON_EXEC,
+					 -1, (struct sockaddr*)&listen_on_addr, socklen);
+      logger_info("server is up and running %s:%s connecting %s:%s",
+		  o.server_addr, o.server_port, o.local_addr, o.local_port);      
+    }
+
   if (!listener) {
     logger_err("bind");
     event_base_free(base);
-    exit(EXIT_FAILURE);    
+    return 1;
   }
-
-  logger_info("%s:%s", o.host, o.port);
-  logger_info("level=%d", verbose);
-
+  
   signal_event = event_new(base, SIGINT,
 			   EV_SIGNAL|EV_PERSIST, signal_func, (void*)base);
 
-  if (!signal_event || event_add(signal_event, NULL)) {
+  if (!signal_event || event_add(signal_event, NULL))
     logger_errx(1, "Cannot add a signal_event");
-  }
+  
+  logger_info("this is `%s` mode", yes_this_is_local ? "local" : "non-local");
   
   event_base_dispatch(base);
+  
   event_base_free(base);
+  
   return 0;
 }
 
@@ -508,15 +602,15 @@ authorize_cb(struct bufferevent *bev, void *ctx)
     return;
   }
   
-  logger_debug(verbose, "auth method=%d;userlen=%d;passwdlen=%d", method, userlen, passwdlen);
+  logger_debug(verbose_flag, "auth method=%d;userlen=%d;passwdlen=%d", method, userlen, passwdlen);
   
   user = calloc(userlen, sizeof(char));
   passwd = calloc(passwdlen, sizeof(char));
   authbuf = calloc(userlen+passwdlen+1, sizeof(char));
   
   authlen = userlen + passwdlen
-                               + 1  /* an extra for the colon */
-                               + 1; /* an extra for the zero  */
+    + 1  /* an extra for the colon */
+    + 1; /* an extra for the zero  */
 
   memcpy(user, buffer+pad, userlen);
   memcpy(passwd, buffer+pad+userlen+1, passwdlen);
