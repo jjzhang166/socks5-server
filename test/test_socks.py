4bdfbe655ae8073ab456e3d3db9a105cfae14e8d
xun <lu.xun@gmx.com>
2018-01-15 20:38:52 +0900

    Add more code
    TODO:
     the process gets hang, so think about an efficent way
     to kill the process; the servers keeps running

--- test/test_socks.py
+++ test/test_socks.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
 # Watch out - this is a work in progress.
-# Simple test for sock servers both local and remote mode
+# Simultiprocessingle test for sock servers both local and remote mode
 # call Curl to see how it goes.
 
 from __future__ import print_function
+
 import os
 import sys
 import shlex
 import subprocess
-import multiprocessing as mp
-import time
+import multiprocessing as multiprocessing
 import signal
 
+from time import time, sleep
 
 ENVFLAGS = {'DEBUG': 'yes'}
 REMOTE_ADDR = "0.0.0.0"
@@ -22,8 +23,8 @@ LOCAL_PORT = 4000
 PASSWORD = "this-is-a-password"
 
 addrs = [
-    "http://google.com",
-    # "https://google.com",
+    # "http://google.com",
+    "https://google.com",
     # "https://ipv6.google.com"
 ]
 
@@ -37,24 +38,39 @@ ERR_CODE = dict(
 )
 
 
-def create_srv_proc(cmd):
+def test_ok(fmt, *args):
+    out = "ok: "
+    out += fmt
+    print(out  % args)
+
+
+def test_failed(fmt, *args):
+    out = "failed: "
+    out += fmt
+    print(out % args)
+
+
+def create_srv_proc(cmd, endtime=None):
     proc = subprocess.Popen(
         shlex.split(cmd), stdin=subprocess.PIPE, stderr=subprocess.PIPE,
         env=ENVFLAGS, cwd="..")
-    
-    _, err = proc.communicate()
-    
-    print(err)
-    
+
+    proc.communicate()
+
+    while True:
+        if time() > endtime: # kill process if this tiemout elapsed            
+            proc.kill()
+            proc.wait()
+            sys.exit(1)
+            # raise(Exception("timeout"))
+        break
+
     return proc
 
 
 def call_curl(cmd):
-    proc = subprocess.Popen(
-         shlex.split(cmd), stdin=subprocess.PIPE, stderr=subprocess.PIPE,
-         env=ENVFLAGS)
-    _, err = proc.communicate()
-    print(err)
+    proc = subprocess.Popen(shlex.split(cmd), env=ENVFLAGS)
+    proc.poll()
     if proc.returncode in ERR_CODE.values():
         i = 0
         while True:
@@ -64,6 +80,8 @@ def call_curl(cmd):
             except IndexError:
                 break
         raise Exception(ERR_CODE.keys()[i-2])
+    elif proc.returncode == 0:
+        test_ok("curl")
 
 
 def call_cmd(cmd):
@@ -72,6 +90,8 @@ def call_cmd(cmd):
         stdin=subprocess.PIPE, stderr=subprocess.PIPE)
     return proc
 
+
+timeout = .3
 
 def start_servers():
     remote = """esocks --server_addr {raddr} --server_port {rport}
@@ -87,43 +107,38 @@ def start_servers():
                raddr=REMOTE_ADDR, rport=REMOTE_PORT,
                password=PASSWORD)
 
-    proc1 = mp.Process(target=create_srv_proc, args=(remote,))
-    setattr(proc1, 'daemon', True) # Run servers background
-    proc1.start()
+    endtime = time() + timeout
     
-    proc2 = mp.Process(target=create_srv_proc, args=(local,))
-    setattr(proc2, 'daemon', True) # Run servers background
+    proc1 = multiprocessing.Process(target=create_srv_proc, args=(remote, endtime))
+    # setattr(proc1, 'daemon', True) # Run server background
+    proc1.start()
+
+    proc2 = multiprocessing.Process(target=create_srv_proc, args=(local, endtime))
+    # setattr(proc2, 'daemon', True) # Run server background
     proc2.start()
 
     return proc1, proc2
 
-
 def test_response(target):
     curl = """curl --socks5 {addr}:{port} {tg} -L -vv""".format(
         addr=LOCAL_ADDR, port=LOCAL_PORT, tg=target)
-    proc3 = mp.Process(target=call_curl, args=(curl,))
+    proc3 = multiprocessing.Process(target=call_curl, args=(curl,))
     proc3.start()    
     assert proc3.is_alive()
 
 
 def test():
     p1, p2 = start_servers()
+
     [test_response(x) for x in addrs]
+    
+    # p1.join(.5)
+    # p2.join(.5)
 
-    # Wait for process completion
-    time.sleep(.4)
-
-    print("remote",p1.pid)
-    print("local",p2.pid)
-
-    p1.terminate()
-    p2.terminate()
-
-    assert not p1.is_alive()
-    assert not p2.is_alive()
+    sleep(.8)
     
     print("stopped!")
 
-    
+
 if __name__ == "__main__":
     test()
