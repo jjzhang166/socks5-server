cad8ad03bd9fef16af0512ce0ed5b9ff38d4ca4b
xun <lu.xun@gmx.com>
2018-01-18 09:13:39 +0900

    Small functions that will help to small tasks

---
+++ evs_helper.c
@@ -0,0 +1,91 @@
+/* helper functions to handle small tasks */
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcip.h>
+#else
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#endif
+
+#include "evs_helper.h"
+#include "evs_log.h"
+#include "evs_internal.h"
+
+
+int
+resolve_host(socks_name_t *n)
+{  
+  struct addrinfo       hints, *res, *p;  
+  struct sockaddr_in                sin;
+  struct sockaddr_in6              sin6;
+  char *host;
+  int i;
+
+  host = (char*)malloc(n->len);
+  
+  if (host == NULL) {
+    free(host);
+    logger_err("malloc");
+    return -1;
+  }
+
+  strcpy(host, n->host);
+
+  logger_debug(DEBUG, "host:\"%s\"", host);
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if (getaddrinfo((char*)host, NULL, &hints, &res) != 0) {
+    free(host);
+    logger_err("host not found");
+    return -1;
+  }
+
+  free(host);
+ 
+  for (i =0, p =res; p != NULL; p = p->ai_next) {
+    switch(p->ai_family) {
+    case AF_INET:
+    case AF_INET6:
+      break;
+    default:
+      continue;
+    }
+    i++;
+  }
+
+  if (i == 0) { /* no results */
+    logger_err("host not found");
+    goto failed;
+  }
+
+  for (p =res; p != NULL; p = p->ai_next) {
+  
+    if (p->ai_family != AF_INET)
+      continue;
+
+    memcpy(&n->sin, p->ai_addr, p->ai_addrlen);
+  
+  }
+
+#ifdef SOCKS_HAVE_INET6
+  for (p =res; p != NULL; p = p->ai_next) {
+    
+    if (p->ai_family != AF_INET6)
+      continue;
+    
+    memcpy(&n->sin6, p->ai_addr, p->ai_addrlen);
+  }
+#endif
+
+  freeaddrinfo(res);  
+  return 0;
+
+ failed:
+  freeaddrinfo(res);
+  return -1;
+}
---
+++ evs_helper.h
@@ -0,0 +1,19 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#ifndef SOCKS_HAVE_INET6 // TODO: configure if system has AF_INET6
+#define SOCKS_HAVE_INET6 1
+#endif
+
+typedef struct {
+  char                   *host;
+  size_t                   len; // host length
+  struct sockaddr_in       sin;
+#if (SOCKS_HAVE_INET6)  
+  struct sockaddr_in6     sin6;
+#endif  
+} socks_name_t;
+
+int resolve_host(socks_name_t *);
+  
+#endif
